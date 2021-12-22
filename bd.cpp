7ec0ab963ad6d4fb1c7f3ceb98790048ec65a548
alexkolodchuk <79657096+alexkolodchuk@users.noreply.github.com>
2021-12-23 00:55:59 +0300

    Update v0.4
    Big fix!
    - Created a ColumnData class rather than just the structure to make it more interactive
    - Changed all strings to dynamic char arrays to prevent memory errors
    - Added all final templates of all the functions
    - Added a detailed description (Imgur image)
    - Fixed AddColumn

--- bd.cpp
+++ bd.cpp
@@ -1,3 +1,10 @@
+/* DBMS Project © Alex Kolodchuk, 2021
+ *
+ * Please do ensure that your compiler supports C++'11 or GNU++'11 standarts
+ *
+ * A repository of this project: https://github.com/alexkolodchuk/dbms
+ * A detailed (Russian) explanation: https://imgur.com/a/7YevnZI
+ */
 #include <iostream>
 #include <locale.h>
 #include <cstdio>
@@ -7,10 +14,39 @@ using namespace std;
 
 /* Initialiation start */
 
-typedef struct TColumnData {
-    string name;
-    int len;
-} ColumnData;
+class ColumnData
+{
+	private:
+		int len;
+		char* name;
+
+	public:
+		ColumnData(int x, string s)
+		{
+			len = x;
+			name = (char*)malloc(len*sizeof(char));
+			for (int i=0; i<len; i++)
+				name[i] = s[i];
+		}
+
+		int Len()
+		{
+			return len;
+		}
+
+		string Name()
+		{
+			string s = "";
+			for (int i=0; i<len; i++)
+				s += name[i];
+			return s;
+		}
+
+		void Clear()
+		{
+		    free(name);
+		}
+};
 
 class Table
 {
@@ -29,49 +65,83 @@ class Table
 			rows = r;
 			columns = c;
 			t = (char***)malloc(rows*sizeof(char**));
-			
+
 			for (int row=0; row<rows; row++) {
 				char** k = (char**)malloc(columns*sizeof(char*));
 				t[row] = k;
-				
+
 				for (int cell=0; cell<columns; cell++) {
-					char* k = (char*)malloc(d[cell].len*sizeof(char));
+					char* k = (char*)malloc(d[cell].Len()*sizeof(char));
 					t[row][cell] = k;
 				}
 			}
-			
+
 			data = (ColumnData*)malloc(columns*sizeof(ColumnData));
-			
+
 			for (int i=0; i<columns; i++)
 				data[i] = d[i];
 		}
-		
+
+		int GetColumns()
+		{
+			return columns;
+		}
+
+		int GetRows()
+		{
+			return rows;
+		}
+
+		ColumnData GetColData(int c)
+		{
+			return data[c];
+		}
+
+		string GetData(int x, int y)
+		{
+            string s = "";
+            for (int i=0; i<data[x].Len(); i++)
+                s += t[y][x][i];
+            return s;
+		}
+
+
+		void SetColData(int n, ColumnData d)
+		{
+		    // In progress...
+		}
+
+		void AddRow(int y)
+		{
+            // In progress...
+		}
+
 		// Column-setter
 		void AddColumn(int id, ColumnData c)
 		{
 			// So, the table is being cached, then cleared and reinitialized
 			// As well as the ColumnData array
-			
+
 			/* Start of initializing cache arrays */
 			char*** cache = (char***)malloc(rows*sizeof(char**));
 			for (int row=0; row<rows; row++) {
 				char** k = (char**)malloc(columns*sizeof(char*));
 				cache[row] = k;
-				
+
 				for (int cell=0; cell<columns; cell++) {
-					char* k = (char*)malloc(data[cell].len*sizeof(char));
+					char* k = (char*)malloc(data[cell].Len()*sizeof(char));
 					cache[row][cell] = k;
-					
-					for (int i=0; i<data[cell].len; i++)
+
+					for (int i=0; i<data[cell].Len(); i++)
 						cache[row][cell][i] = t[row][cell][i];
 				}
 			}
-			
+
 			ColumnData* dataCache = (ColumnData*)malloc(columns*sizeof(ColumnData));
 			for (int i=0; i<columns; i++)
 				dataCache[i] = data[i];
 			/* End of initializing cache arrays */
-			
+
 			// Clearing away all of the memory contained in the main table after the id-column and it itself.
 			for (int row=0; row<rows; row++) {
 				for (int cell=0; cell<columns; cell++) {
@@ -79,11 +149,10 @@ class Table
 				}
 				free(t[row]);
 			}
-			free(t);
 			free(data);
-			
+
 			columns++;
-			
+
 			// Rewriting data to ColumnData array, then clearing away cache array
 			data = (ColumnData*)malloc(columns*sizeof(ColumnData));
 			for (int i=0; i<columns; i++) {
@@ -92,62 +161,60 @@ class Table
 				else data[i] = dataCache[i+1];
 			}
 			free(dataCache);
-			
+
 			// Rewriting data to main table array, then clearing away cache array
 			for (int row=0; row<rows; row++) {
 				char** k = (char**)malloc(columns*sizeof(char*));
 				t[row] = k;
 				for (int cell=0; cell<columns; cell++) {
-					char* k = (char*)malloc(data[cell].len*sizeof(char));
+					char* k = (char*)malloc(data[cell].Len()*sizeof(char));
 					t[row][cell] = k;
 					if (cell<id) {
 						for (int i=0; i<cell; i++)
 							t[row][cell][i] = cache[row][cell][i];
-							
+
 					} else if (cell==id) {
 						for (int i=0; i<cell; i++)
 							t[row][cell][i] = '1';
-							
+
 					} else if (cell>id) {
 						for (int i=0; i<cell; i++)
 							t[row][cell][i] = cache[row][cell-1][i];
 					}
 				}
 			}
-			
+
 			for (int row=0; row<rows; row++) {
-				for (int cell=0; cell<columns-1; cell++) 
+				for (int cell=0; cell<columns-1; cell++)
 					free(cache[row][cell]);
 				free(cache[row]);
 			}
 			free(cache);
 		}
-		
-		int GetColumns()
-		{
-			return columns;
-		}
-		
-		void SetRows(int r)
-		{
-			// In progress...
-		}
-		
-		int GetRows()
-		{
-			return rows;
-		}
-		
-		void SetColData(int n, ColumnData d)
-		{
-			data[n] = d;
-			// In progress...
-		}
-		
-		ColumnData GetColData(int c)
-		{
-			return data[c];
-		}
+
+        void SetData(int x, int y, char* d)
+        {
+            for (int i=0; i<data[x].Len(); i++)
+                t[y][x][i] = d[i];
+            free(d);
+        }
+
+        void SortBy(int x, bool mode)
+        {
+            // In progress...
+        }
+
+        void Clear()
+        {
+            for (int row=0; row<rows; row++) {
+				for (int cell=0; cell<columns; cell++) {
+					free(t[row][cell]);
+				}
+				free(t[row]);
+			}
+			free(t);
+			free(data);
+        }
 };
 
 /* Initialization end */
@@ -169,84 +236,79 @@ int main()
 	// Mainloop
     string i;
     while (true) {
-		
+
 		// Main menu
-        lang? cout << "Welcome to MephDBMS.\n"                 : cout << "–î–æ–±—Ä–æ –ø–æ–∂–∞–ª–æ–≤–∞—Ç—å –≤ –ú–∏—Ñ–°–£–ë–î.\n";
-        lang? cout << "To create new spreadsheet, press N.\n"  : cout << "–ß—Ç–æ–±—ã —Å–æ–∑–¥–∞—Ç—å –Ω–æ–≤—É—é –±–∞–∑—É, –Ω–∞–∂–º–∏—Ç–µ N.\n";
-        lang? cout << "To open an already existing, press O.\n": cout << "–ß—Ç–æ–±—ã –æ—Ç–∫—Ä—ã—Ç—å —Å—É—â–µ—Å—Ç–≤—É—é—â—É—é, –Ω–∞–∂–º–∏—Ç–µ O.\n";
-        lang? cout << "To open preferences, press P.\n"        : cout << "–ß—Ç–æ–±—ã –æ—Ç–∫—Ä—ã—Ç—å –æ–∫–Ω–æ –Ω–∞—Å—Ç—Ä–æ–µ–∫, –Ω–∞–∂–º–∏—Ç–µ P.\n";
-        lang? cout << "To close MephDBMS, press Q.\n"          : cout << "–ß—Ç–æ–±—ã –≤—ã–π—Ç–∏ –∏–∑ –ú–∏—Ñ–°–£–ë–î, –Ω–∞–∂–º–∏—Ç–µ Q.\n";
+        lang? cout << "Welcome to MephDBMS.\n"                 : cout << "ƒÓ·Ó ÔÓÊ‡ÎÓ‚‡Ú¸ ‚ ÃËÙ—”¡ƒ.\n";
+        lang? cout << "To create new spreadsheet, press N.\n"  : cout << "◊ÚÓ·˚ ÒÓÁ‰‡Ú¸ ÌÓ‚Û˛ ·‡ÁÛ, Ì‡ÊÏËÚÂ N.\n";
+        lang? cout << "To open an already existing, press O.\n": cout << "◊ÚÓ·˚ ÓÚÍ˚Ú¸ ÒÛ˘ÂÒÚ‚Û˛˘Û˛, Ì‡ÊÏËÚÂ O.\n";
+        lang? cout << "To open preferences, press P.\n"        : cout << "◊ÚÓ·˚ ÓÚÍ˚Ú¸ ÓÍÌÓ Ì‡ÒÚÓÂÍ, Ì‡ÊÏËÚÂ P.\n";
+        lang? cout << "To close MephDBMS, press Q.\n"          : cout << "◊ÚÓ·˚ ‚˚ÈÚË ËÁ ÃËÙ—”¡ƒ, Ì‡ÊÏËÚÂ Q.\n";
 
 		// Symbol reading
         cin >> i;
         if (system("cls")==32512) system("clear");
-        
-        if (i=="n" || i=="N" || i=="—Ç" || i=="–¢") {
+
+        if (i=="n" || i=="N" || i=="Ú" || i=="“") {
             /* DB initialization and working with it */
-                        
+
             int sizeC, sizeR;
-            lang? cout << "Enter initial quantity of columns: " : cout << "–í–≤–µ–¥–∏—Ç–µ –Ω–∞—á–∞–ª—å–Ω–æ–µ –∫–æ–ª-–≤–æ —Å—Ç–æ–ª–±—Ü–æ–≤: ";
+            lang? cout << "Enter initial quantity of columns: " : cout << "¬‚Â‰ËÚÂ Ì‡˜‡Î¸ÌÓÂ ÍÓÎ-‚Ó ÒÚÓÎ·ˆÓ‚: ";
             cin >> sizeC;
-            ColumnData cols[sizeC];
+            ColumnData* cols = (ColumnData*)malloc(0);
             for (int i=0; i<sizeC; i++) {
-				lang? cout << "\nEnter the name of the column ‚Ññ" << i << ": " : cout << "–í–≤–µ–¥–∏—Ç–µ –Ω–∞–∑–≤–∞–Ω–∏–µ —Å—Ç–æ–ª–±—Ü–∞ ‚Ññ" << i+1 << ": ";
-				cin >> cols[i].name;
-				getline(cin, cols[i].name);
-				lang? cout << "\nEnter the length of the column ‚Ññ" << i << ": " : cout << "–í–≤–µ–¥–∏—Ç–µ –¥–ª–∏–Ω—É —Å—Ç–æ–ª–±—Ü–∞ ‚Ññ" << i+1 << ": ";
-				cin >> cols[i].len;
+            	string name; int len;
+				lang? cout << "\nEnter the name of the column π" << i << ": " : cout << "¬‚Â‰ËÚÂ Ì‡Á‚‡ÌËÂ ÒÚÓÎ·ˆ‡ π" << i+1 << ": ";
+				cin >> name;
+				getline(cin, name);
+				lang? cout << "\nEnter the length of the column π" << i << ": " : cout << "¬‚Â‰ËÚÂ ‰ÎËÌÛ ÒÚÓÎ·ˆ‡ π" << i+1 << ": ";
+				cin >> len;
+
+                realloc(cols, (i+1)*sizeof(ColumnData));
+                ColumnData k(len, name);
+				cols[i] = k;
 			}
-			lang? cout << "Enter initial quantity of rows: " : cout << "–í–≤–µ–¥–∏—Ç–µ –Ω–∞—á–∞–ª—å–Ω–æ–µ –∫–æ–ª-–≤–æ —Å—Ç—Ä–æ–∫: ";
+			lang? cout << "Enter initial quantity of rows: " : cout << "¬‚Â‰ËÚÂ Ì‡˜‡Î¸ÌÓÂ ÍÓÎ-‚Ó ÒÚÓÍ: ";
             cin >> sizeR;
-            
+
             Table t(sizeR, sizeC, cols);
-            ColumnData f; f.len = 1; f.name = "a";
-            t.AddColumn(1, f);
-            
-            
-        } else if (i=="o" || i=="O" || i=="—â" || i=="–©") {
+
+            // In progress...
+
+            //ColumnData f(1, "a");
+            //t.AddColumn(1, f);
+
+
+        } else if (i=="o" || i=="O" || i=="˘" || i=="Ÿ") {
             /* DB opening and work with it */
+            // In progress...
         }
-        else if (i=="p" || i=="P" || i=="–∑" || i=="–ó") {
+        else if (i=="p" || i=="P" || i=="Á" || i=="«") {
             /* Settings menu */
 
 			while (true) {
-				lang? cout << "Preferences:\n"          : cout << "–ù–∞—Å—Ç—Ä–æ–π–∫–∏:\n";
-				lang? cout << "[1] Language: English\n" : cout << "[1] –Ø–∑—ã–∫: –†—É—Å—Å–∫–∏–π\n";
-				lang? cout << "[2] Keybindings...\n"    : cout << "[2] –£–ø—Ä–∞–≤–ª–µ–Ω–∏–µ –∫–ª–∞–≤–∏—à–∞–º–∏...\n";
-				lang? cout << "[3] Reset all binds\n"   : cout << "[3] –°–±—Ä–æc —É–ø—Ä–∞–≤–ª–µ–Ω–∏—è\n";
-				lang? cout << "[4] Quit to main menu\n" : cout << "[4] –í—ã—Ö–æ–¥ –≤ –≥–ª–∞–≤–Ω–æ–µ –º–µ–Ω—é\n";
+				lang? cout << "Preferences:\n"          : cout << "Õ‡ÒÚÓÈÍË:\n";
+				lang? cout << "[1] Language: English\n" : cout << "[1] ﬂÁ˚Í: –ÛÒÒÍËÈ\n";
+				lang? cout << "[2] Keybindings...\n"    : cout << "[2] ”Ô‡‚ÎÂÌËÂ ÍÎ‡‚Ë¯‡ÏË...\n";
+				lang? cout << "[3] Reset all binds\n"   : cout << "[3] —·Óc ÛÔ‡‚ÎÂÌËˇ\n";
+				lang? cout << "[4] Quit to main menu\n" : cout << "[4] ¬˚ıÓ‰ ‚ „Î‡‚ÌÓÂ ÏÂÌ˛\n";
 				cin >> i;
 				if (system("cls")==32512) system("clear");
-				
+
 				if (i=="1") {
-					lang? cout << "Change program language to Russian?\n[y/n] " : cout << "–ò–∑–º–µ–Ω–∏—Ç—å —è–∑—ã–∫ –ø—Ä–æ–≥—Ä–∞–º–º—ã –Ω–∞ –ê–Ω–≥–ª–∏–π—Å–∫–∏–π?\n[y/n] ";
+					lang? cout << "Change program language to Russian?\n[y/n] " : cout << "»ÁÏÂÌËÚ¸ ˇÁ˚Í ÔÓ„‡ÏÏ˚ Ì‡ ¿Ì„ÎËÈÒÍËÈ?\n[y/n] ";
 					cin >> i;
-					if (i=="y" || i=="Y" || i=="–Ω" || i=="–ù") {lang = !lang; continue;}
+					if (i=="y" || i=="Y" || i=="Ì" || i=="Õ") {lang = !lang; continue;}
 				} else if (i=="2") {
 					// In progress..
-					
-					// Create new db: N
-					// Open db: O
-					// Open prefs: P
-					// Save: S
-					// Quit: Q
-					
-					// Go up: W
-					// Go left: A
-					// Go down: S
-					// Go right: D
-					// Edit cell: E
-					// Reset cell: R
-					// Find in db: F
-					// New row: Z
-					// New column: X
+
 				} else if (i=="3") {
 					// In progress..
+
 				} else if (i=="4")
 					break;
 			}
         }
-        else if (i=="q" || i=="Q" || i=="–π" || i=="–ô") return 0;
+        else if (i=="q" || i=="Q" || i=="È" || i=="…") return 0;
     }
 
     return 0;
